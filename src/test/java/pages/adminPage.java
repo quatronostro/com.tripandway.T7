729c8bc770fa9650afbdea1ed79a7ca1a05e4dc7
Berke <93223660+quatronostro@users.noreply.github.com>
2023-06-02 23:10:28 +0300

    berke 02/06 v.1

--- src/test/java/pages/adminPage.java
+++ src/test/java/pages/adminPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,5 +9,7 @@ public class adminPage {
 
     public adminPage(){PageFactory.initElements(Driver.getDriver(), this);}
 
-
+     //AdminPage >>> SideBar >>> AccordionButton
+    @FindBy(id = "accordionSidebar")
+    public WebElement accordionButton;
 }
---
+++ src/test/java/tests/berke_US_31_35_36_40/US_040.java
@@ -0,0 +1,18 @@
+package tests.berke_US_31_35_36_40;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US_040 {
+
+    @Test
+    public void TC_01(){
+        //1. Webdriver çalıştırılarak "https://qa.tripandway.com/admin" sayfasına gidilir ve admin hesap bilgileri ile giriş yapılır
+        Driver.getDriver().get(ConfigReader.getProperty("tawAdminUrl"));
+
+        //2. Sol tarafta bulunan panelin alt kısmında ok işaretli butona tıklanır
+
+        //3. Panelin kapalı hale geldiği doğrulanır
+    }
+}
