e12edc07551d4c074720d673be03bfe9d9341e99
Berke <93223660+quatronostro@users.noreply.github.com>
2023-06-03 16:35:11 +0300

    berke 03/06 v.4

--- src/test/java/pages/TawAdminPage.java
+++ src/test/java/pages/TawAdminPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class TawAdminPage {
@@ -10,6 +11,8 @@ public class TawAdminPage {
     public TawAdminPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    public static TawAdminPage tawAdminPage;
 
      //AdminPage >>> SideBar >>> AccordionButton
     @FindBy(id = "accordionSidebar")
@@ -40,5 +43,28 @@ public class TawAdminPage {
     @FindBy(xpath = "//*[@*='sidebar-toggled']")
     public WebElement sideBarToggledLocate;
 
+    public static void tawAdminAccountGiris(){
 
+        tawAdminPage = new TawAdminPage();
+
+        tawAdminPage.adminEmailBox.sendKeys(ConfigReader.getProperty("adminEmail1"));
+        tawAdminPage.adminPassBox.sendKeys(ConfigReader.getProperty("adminPass"));
+        tawAdminPage.adminLoginButton.click();
+    }
+
+    //AdminPage >>> SolPanelList >>> PackagesButton
+    @FindBy(xpath = "//*[@id=\"accordionSidebar\"]/li[7]/a/i")
+    public WebElement packagesButton;
+
+    //AdminPage >>> SolPanelList >>> Packages >>> AddNewButton
+    @FindBy(xpath = "//*[@id=\"content\"]/div/div/div[1]/div/a")
+    public WebElement packagesAddNewButton;
+
+    //AdminPage >>> SolPanelList >>> Packages >>> AddNewButton >>> PackagesNameBox
+    @FindBy(xpath = "//*[@*='p_name']")
+    public WebElement packagesNameBox;
+
+    //AdminPage >>> SolPanelList >>> Packages >>> AddNewPackagesPage >>> ChooseFileButton
+    @FindBy(xpath = "//*[@*='p_photo']")
+    public  WebElement packagesChooseFileButton;
 }
