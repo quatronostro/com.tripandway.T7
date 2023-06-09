e222c22e87f1ff68572a482c53876d5295d0cb18
pc <m.emreatci@gmail.com>
2023-06-09 17:27:02 +0300

    mustafa 09.06/v.1

---
+++ src/test/java/tests/mustafa_US_16_23_27_30/US_23.java
@@ -0,0 +1,45 @@
+package tests.mustafa_US_16_23_27_30;
+
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TawUserHomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US_23 {
+    TawUserHomePage tawUserHomePage;
+
+    @Test
+    public void Test01() {
+
+        tawUserHomePage = new TawUserHomePage();
+        Actions actions = new Actions(Driver.getDriver());
+
+        //1.Webdriver çalıştırılarak https://www.tripandway.com/qa/ sayfasına gidilir ve login linki tıklanarak kullanıcı girişi yapılır.
+        Driver.getDriver().get(ConfigReader.getProperty("tawUrl"));
+
+        tawUserHomePage.homePageLoginLink.click();
+
+        TawUserHomePage.tawHomePageAccountGiris();
+
+        //2.Logout butonunun görünür olduğu doğrulanır.
+        Assert.assertTrue(tawUserHomePage.loginPageLogoutButton.isDisplayed());
+
+        //3.Logout butonu tıklanır.
+        tawUserHomePage.loginPageLogoutButton.click();
+
+        //4.Açılan yeni sayfanın "Dashboard" sayfası olmadığı, login sayfası olduğu doğrulanır.
+        String ecpectedContain ="login";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(ecpectedContain),"Login sayfasında degilsiniz !");
+
+
+        //5.Açılan sayfada Login butonunun aktif olduğu doğrulanır.
+        Assert.assertTrue(tawUserHomePage.homePageLoginButton.isDisplayed());
+
+        Driver.closeDriver();
+    }
+}
+//m.taskiran1@outlook.com
+// Mt7123489
